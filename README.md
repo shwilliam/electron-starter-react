f6bb163ba2c3e238404478935b9cb8a15d66e9c7
William L <w-lindvall@outlook.com>
2020-03-06 02:11:44 -0800

    init electron

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-#electron-starter-react
+# electron-starter-react
+
+> Electron + Create React App Boilerplate
+
+## Development
+
+1. `yarn`
+2. `yarn start`
+
+## Production
+
+`yarn build`
--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {render} from '@testing-library/react'
-import App from './App'
-
-test('renders hello world', () => {
-  const {getByText} = render(<App />)
-  const helloEl = getByText(/hello world/i)
-  expect(helloEl).toBeInTheDocument()
-})
